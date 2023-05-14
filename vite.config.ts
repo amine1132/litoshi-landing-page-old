8fe64a064b75dc9b2a85dd4b08671fe096b334b0
Amine Sekkiou <amine1132@hotmail.fr>
2023-05-14 14:53:22 +0200

    Dernier ajout de fichier

--- vite.config.ts
+++ vite.config.ts
@@ -5,4 +5,9 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   server: { watch: { awaitWriteFinish: true } },
   plugins: [react()],
+  build: {
+    chunkSizeWarningLimit: 1000000,
+    // Other build configuration options
+  } 
 });
+
